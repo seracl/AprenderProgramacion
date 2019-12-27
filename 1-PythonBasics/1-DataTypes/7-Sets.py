8257d2878bafa3435ca163a4abc603eb1ee5a2ab
Raul Clark <juan.raul.clark@gmail.com>
2019-12-27 22:31:34 +0100

    Sets

---
+++ 1-PythonBasics/1-DataTypes/7-Sets.py
@@ -0,0 +1,12 @@
+### Sets     
+'''
+Unordered collection of unique objects (there can only be one representative of the same object)
+'''
+myset = set()
+myset.add(int(1))
+myset.add(int(2))
+myset.add(int(1))
+myset.add(str(a))
+print('Sets:')
+print('myset = ', myset)
+print('\n') 
