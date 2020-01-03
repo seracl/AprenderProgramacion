39be35104684e70fbbad016cdc74dfafb25d4718
Raul Clark <raul.clark@habo.se>
2020-01-03 12:19:28 +0100

    DS

---
+++ 2-DataStructures/1-General.py
@@ -0,0 +1,29 @@
+# Data Structure and Algorithms
+"""
+https://www.tutorialspoint.com/data_structures_algorithms/data_structures_basics.htm
+Data Structures are the programmatic way of storing data so that data can be used efficiently.
+Almost every enterprise application uses various types of data structures in one or the other way.
+
+Algorithm is a step-by-step procedure, which defines a set of instructions to be 
+	executed in a certain order to get the desired output. 
+	Algorithms are generally created independent of underlying languages, i.e. 
+		an algorithm can be implemented in more than one programming language.
+
+## Why to Learn Data Structure and Algorithms?
+As applications are getting complex and data rich. Which carry some problems:
+- Data Search
+- Processor speed: Processor speed although being very high, falls limited if the data grows to billion records.
+- Multiplr requests
+
+To solve the above-mentioned problem, data can be organized in such a way
+	that items might be searched instantly
+
+## Applications of Data Structure and Algorithms
+Categories of algorithms, from the data structure point of view:
+- Search
+- Sort
+- Insert
+- Update
+- Delete
+The following problems ca
+"""
