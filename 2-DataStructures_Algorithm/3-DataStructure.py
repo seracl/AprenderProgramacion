66022cffe592609d72910f3cfec1930777f72fc3
Raul Clark <raul.clark@habo.se>
2020-01-07 15:08:48 +0100

    January 7 update

---
+++ 2-DataStructures_Algorithm/3-DataStructure.py
@@ -0,0 +1,22 @@
+## Data Structures
+"""
+Data Structures: Programmatic way of storing data so that data can be used efficiently.
+
+Data structures foundation terms:
+	- Interface: Each data structure has an interface, or set of operations, accepted parameters and return types that a data structure supports. 
+	- Implementation: Implementation provides the internal representation and the definition of the algorithms used in the operations of the data structure. 
+
+Characteristics of a Data Structure
+	- Correctness: Data structure implementation should implement its interface correctly.
+	- Time Complexity: Running time or the execution time of operations of data structure must be as small as possible.
+	- Space Complexity: Memory usage of a data structure operation should be as little as possible.
+
+Execution Time Cases: Used to compare data structure's relative execution time (f(n) execution time in function of the f(data) ).
+	- Worst Case: Maximum amount of time a data structure operation time can take. 
+		In worst case, operations won't take more than ƒ(n) time where ƒ(n) represents function of n.
+	- Average Case: Average execution time of an operation of a data structure. 
+		If an operation takes ƒ(n) time in execution, then m operations will take m*ƒ(n) time.
+	- Best Case: An operation's least possible execution time.
+		If execution takes ƒ(n) time, then the actual operation may take time as the random number with ƒ(n) maximum time.
+
+"""
